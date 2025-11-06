7c687e983d76bbdf0a1a2e722f5cd427211a8f63
murali-tech1 <bttmuralimohan@gmail.com>
2025-11-06 13:21:54 +0530

    addtion.js

---
+++ addition.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("enter the first number"));
+let num2 = parseFloat(prompt("enter the second number"));
+
+let sum = num1 + num2;
+
+alert("the sum is : " + sum);
+console.log("the sum is: " + sum);
